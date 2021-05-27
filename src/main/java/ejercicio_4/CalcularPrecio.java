46190be0b355af4327e16705881850a03d30a32d
Efren leonarso sosa <efrenl.sosab@gmail.com>
2021-05-27 15:37:21 -0500

    se agrega punto 4

--- src/main/java/ejercicio_4/CalcularPrecio.java
+++ src/main/java/ejercicio_4/CalcularPrecio.java
@@ -4,11 +4,9 @@ public class CalcularPrecio {
     private final double iva = 0.21;
     private double total = 0.0;
     public void calculoPrecio(double precio){
-        total = precio * iva;
+        total = precio + precio * iva;
         System.out.println("El precio total del producto es: " + total + ".");
 
     }
-    public double calcularIva(double precio){
-        
-    }
+
 }
